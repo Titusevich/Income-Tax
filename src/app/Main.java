e663b0dfb924c7553123d92af98d7fdc2a54a65a
Titusevich <nickitatitaevskiy@gmail.com>
2024-12-13 15:14:30 +0200

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,10 @@
+package app;
+
+public class Main {
+    public static double income;
+    public static double taxRate1;
+    public static double taxRate2;
+    public static double taxRate3;
+    private static final String CURRENCY = "UAH";
+    public static double taxSum;
+}
