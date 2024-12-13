c33e1e43bcd24ee03a921cd7eedd7272b6d35d4a
Titusevich <nickitatitaevskiy@gmail.com>
2024-12-13 15:25:31 +0200

    Add third income and tax condition

--- src/app/Main.java
+++ src/app/Main.java
@@ -24,6 +24,11 @@ public class Main {
             System.out.printf("%nIncome %.2f %s is taxed at %.1f%% rate.%n" +
                             "The sum of tax is %.2f %s.%n",
                     income, CURRENCY, taxRate2, taxSum, CURRENCY);
+        }else if (income > 25000) {
+            taxSum = (income * taxRate3) / 100;
+            System.out.printf("%nIncome %.2f %s is taxed at %.1f%% rate.%n" +
+                            "The sum of tax is %.2f %s.%n",
+                    income, CURRENCY, taxRate3, taxSum, CURRENCY);
         }
     }
 }
